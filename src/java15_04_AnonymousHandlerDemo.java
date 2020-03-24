97652a921a0b0eb63319398457732d40f7e5b11d
Mr <lsjsss@live.cn>
2020-03-24 16:06:07 +0800

    3.24 16 - update for 3.24 class

--- src/java15_04_AnonymousHandlerDemo.java
+++ src/java15_04_AnonymousHandlerDemo.java
@@ -1,4 +1,3 @@
-import com.sun.corba.se.impl.resolver.SplitLocalResolverImpl;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,9 +7,11 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+/**使用匿名内部类来编写事件驱动程序*/
 public class java15_04_AnonymousHandlerDemo extends Application {
     @Override
     public void start(Stage PrimaryStage){
+        //图形构造
         HBox hBox = new HBox();
         hBox.setSpacing(10);
         hBox.setAlignment(Pos.CENTER);
@@ -20,12 +21,18 @@ public class java15_04_AnonymousHandlerDemo extends Application {
         Button btPrint = new Button("Print");
         hBox.getChildren().addAll(btNew, btOpen, btSave, btPrint);
 
+        //给 new 按钮设置监听
         btNew.setOnAction(new EventHandler<ActionEvent>() {
             @Override
+            //匿名内部类
             public void handle(ActionEvent event) {
                 System.out.println("Process New");
             }
         });
+        //btNew.setOnAction(.....）;
+        //new  的后面是一个 EventHandler 接口，监听器必须实现的接口 泛型<ActionEvent>
+        //()  无参的
+        //{}里 实际上是原来内部类的类体
 
         btOpen.setOnAction(new EventHandler<ActionEvent>() {
             @Override
