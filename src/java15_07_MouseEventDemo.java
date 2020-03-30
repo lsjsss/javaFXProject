7d5169af505a0a0017bf3bcb5b55ead6347f5fdf
Mr <lsjsss@live.cn>
2020-03-30 14:45:20 +0800

    3.30 14 - update for 3.30 class - MouseEvent

--- src/java15_07_MouseEventDemo.java
+++ src/java15_07_MouseEventDemo.java
@@ -8,11 +8,18 @@ public class java15_07_MouseEventDemo extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
+        //文本初始位置(坐标)，内容
         Text text = new Text(20, 20, "Programming is fun");
         pane.getChildren().addAll(text);
+        //监听不是动作事件(ActionEvent) 是监听鼠标事件(MouseEvent)
+        //事件源是 text， 监听鼠标拖动事件
         text.setOnMouseDragged(e -> {
+            //将文本框的x， y坐标设置为鼠标拖动的坐标
             text.setX(e.getX());
             text.setY(e.getY());
+            //画线程序
+            //text->ImageView
+            //绘制线条 Panit 类，画点(多个点)
         });
 
         Scene scene = new Scene(pane, 300, 100);
