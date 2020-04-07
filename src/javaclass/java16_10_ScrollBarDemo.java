963fc638bf983af1f6ad4791fd2023ea779aa2cd
Mr <lsjsss@live.cn>
2020-04-07 14:54:40 +0800

    4.7 14 - update for 4.7 class

--- src/javaclass/java16_10_ScrollBarDemo.java
+++ src/javaclass/java16_10_ScrollBarDemo.java
@@ -14,27 +14,38 @@ public class java16_10_ScrollBarDemo extends Application {
     public void start(Stage primaryStage) throws Exception {
         Text text = new Text(20, 20, "JavaFX Programming");
 
+        //水平滚动条
         ScrollBar sbHorizontal = new ScrollBar();
+        //垂直滚动条(默认为水平)
         ScrollBar sbVertical = new ScrollBar();
         sbVertical.setOrientation(Orientation.VERTICAL);
 
+        System.out.println("Min" + sbHorizontal.getMin());
+        System.out.println("Max" + sbHorizontal.getMax());
+
         Pane paneForText = new Pane();
+        //将文本放到面板上
         paneForText.getChildren().add(text);
 
+        //边界面板
         BorderPane pane = new BorderPane();
         pane.setCenter(paneForText);
         pane.setBottom(sbHorizontal);
         pane.setRight(sbVertical);
 
+        //为水平滚动条添加监听 值属性      监听
         sbHorizontal.valueProperty().addListener(ov ->
+                //修改文本的 x 坐标与滚动条相同（依照面板比例）
                 text.setX(sbHorizontal.getValue() * paneForText.getWidth() / sbHorizontal.getMax()));
 
         sbVertical.valueProperty().addListener(ov ->
                 text.setY(sbVertical.getValue() * paneForText.getHeight() / sbVertical.getMax()));
 
+        //场景
         Scene scene = new Scene(pane, 450, 170);
         primaryStage.setTitle("ScrollBarDemo");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
+//水平滚动条的最小值和最大值，单击按钮时移动的距离，单极滑块的轨道空白处移动的距离
