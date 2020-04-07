4cfd0fe05d642ff4dadd7d8a985be5a6ddccc2d2
Mr <lsjsss@live.cn>
2020-04-07 15:42:15 +0800

    4.7 16 - update for 4.7 class

--- src/javaclass/java16_14_MediaDemo.java
+++ src/javaclass/java16_14_MediaDemo.java
@@ -18,33 +18,41 @@ import javafx.util.Duration;
 
 public class java16_14_MediaDemo extends Application {
     private static final String MEDIA_URL =
-            "http://cs.armstrong.edu/liang/common/sample.mp4";
+            "http://47.95.255.91/jiongma.mp4";
+//                    "cs.armstrong.edu/liang/common/sample.mp4";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Media media = new Media(MEDIA_URL);
         MediaPlayer mediaPlayer = new MediaPlayer(media);
+        //mediaPlayer做参数，MediaView做对象
         MediaView mediaView = new MediaView(mediaPlayer);
 
+        //播放按钮
         Button playButton = new Button(">");
         playButton.setOnAction(e -> {
             if (playButton.getText().equals(">")) {
                 mediaPlayer.play();
+                //单击播放按钮变为暂停状态
                 playButton.setText("||");
             } else {
                 mediaPlayer.play();
+                //单击暂停按钮变为播放状态
                 playButton.setText(">");
             }
         });
 
         Button rewindButton = new Button("<<");
+        //按快退键转到视频开头
         rewindButton.setOnAction(e -> mediaPlayer.seek(Duration.ZERO));
 
+        //滑杆，调节音量
         Slider slVolume = new Slider();
         slVolume.setPrefWidth(150);
         slVolume.setMaxWidth(Region.USE_COMPUTED_SIZE);
         slVolume.setMinWidth(30);
         slVolume.setValue(50);
+        //值属性除100，与视频音量属性相同
         mediaPlayer.volumeProperty().bind(
                 slVolume.valueProperty().divide(100));
 
