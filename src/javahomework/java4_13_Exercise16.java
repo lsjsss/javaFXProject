4e8784f579585116b77e586c39f108c5190a2648
Mr <lsjsss@live.cn>
2020-04-13 01:00:37 +0800

    4.13 01 - update java homework

--- src/javahomework/java4_13_Exercise16.java
+++ src/javahomework/java4_13_Exercise16.java
@@ -1,4 +1,12 @@
 package javahomework;
 
-public class java4_13_Exercise16 {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class java4_13_Exercise16 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
