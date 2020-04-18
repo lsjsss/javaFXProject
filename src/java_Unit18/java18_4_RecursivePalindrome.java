27251bcc421760f7165501670c67ad93f318b949
Mr <lsjsss@live.cn>
2020-04-19 01:20:13 +0800

    4.19 01 - update Unit 18

--- src/java_Unit18/java18_4_RecursivePalindrome.java
+++ src/java_Unit18/java18_4_RecursivePalindrome.java
@@ -1,15 +1,19 @@
 package java_Unit18;
 
-public class java18_3_RecursivePalindromeUsingSubstring {
+public class java18_4_RecursivePalindrome {
     public static boolean isPalindrome(String s) {
-        if (s.length() <= 1) {
+        return isPalindrome(s, 0, s.length() - 1);
+    }
+
+    private static boolean isPalindrome(String s, int low, int high) {
+        if (high <= low) {
             // Base case
             return true;
-        } else if (s.charAt(0) != s.charAt(s.length() - 1)) {
+        } else if (s.charAt(low) != s.charAt(high)) {
             // Base case
             return false;
         } else {
-            return isPalindrome(s.substring(1, s.length() - 1));
+            return isPalindrome(s, low + 1, high - 1);
         }
     }
 
