6259a0becaf470bcc9fef697098284e74654b7d2
Mr <lsjsss@live.cn>
2020-04-19 01:33:01 +0800

    4.19 01 - update Unit 18

---
+++ src/java_Unit18/java18_7_DirectorySize.java
@@ -0,0 +1,33 @@
+package java_Unit18;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class java18_7_DirectorySize {
+    public static void main(String[] args) {
+        // Prompt the user to enter a directory or a file
+        System.out.print("Enter a directory or a file: ");
+        Scanner input = new Scanner(System.in);
+        String directory = input.nextLine();
+        // Display the size
+        System.out.println(getSize(new File(directory)) + " bytes");
+    }
+
+    public static long getSize(File file) {
+        // Store the total size of all files
+        long size = 0;
+        if (file.isDirectory()) {
+            // All files and subdirectories
+            File[] files = file.listFiles();
+            for (int i = 0; files != null && i < files.length; i++) {
+                // Recursive call
+                size += getSize(files[i]);
+            }
+        } else {
+            // Base case
+            size += file.length();
+        }
+        return size;
+    }
+
+}
