313482f65ee4e464a3c108a7309b21d9e98b1e44
Mr <lsjsss@live.cn>
2020-04-21 14:29:19 +0800

    4.21 14 - update 4.21 class

---
+++ src/java_Unit19/java19_3_BoundedTypeDemo.java
@@ -0,0 +1,21 @@
+package java_Unit19;
+
+
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+/** 受限的泛型类型， 类型更窄*/
+public class java19_3_BoundedTypeDemo {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2, 2);
+        Circle circle = new Circle(2);
+
+//        System.out.println("Same area? " + equalArea(rectangle, circle));
+    }
+
+    /** 图形对象 */
+//    public static <E extends GeometricObject> boolean equalArea(E object1, E object2) {
+//        return object1.getArea() == object2.getArea();
+//    }
+}
+//E是受限的， <E>其实等价于<W extends Object>
---
+++ src/java_Unit19/java19_4_GenericSort.java
@@ -0,0 +1,79 @@
+package java_Unit19;
+
+public class java19_4_GenericSort {
+    public static void main(String[] args) {
+        // Create an Integer array
+        //创建一个 Integer 类型数组
+        Integer[] intArray = {new Integer(2), new Integer(4),
+                new Integer(3)};
+        // Create a Double array
+        //创建一个Double类型数组
+        Double[] doubleArray = {new Double(3.4), new Double(1.3),
+                new Double(-22.1)};
+        // Create a Character array
+        //创建一个字符类型的数组
+        Character[] charArray = {new Character('a'),
+                new Character('J'), new Character('r')};
+        // Create a String array
+        //创建一个字符串数组
+        String[] stringArray = {"Tom", "Susan", "Kim"};
+        // Sort the arrays
+        //编一段程序，对以上四种类型排序
+        sort(intArray);
+        sort(doubleArray);
+        sort(charArray);
+        sort(stringArray);
+        // Display the sorted arrays
+        System.out.print("Sorted Integer objects: ");
+        printList(intArray);
+        System.out.print("Sorted Double objects: ");
+        printList(doubleArray);
+        System.out.print("Sorted Character objects: ");
+        printList(charArray);
+        System.out.print("Sorted String objects: ");
+        printList(stringArray);
+    }
+
+    /**
+     * Sort an array of comparable objects
+     */
+
+    /**
+     *      * 两个 E 类型对象进行比较(同一类型进行比较)
+     *      * E 是 Comparable 的子类型
+     *      * 指定进行比较的元素是 E 类型
+     */
+    public static <E extends Comparable<E>> void sort(E[] list) {
+        E currentMin;
+        int currentMinIndex;
+        for (int i = 0; i < list.length - 1;
+             i++) {
+            // Find the minimum in the list[i+1..list.length?2]
+            currentMin = list[i];
+            currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin.compareTo(list[j]) > 0) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            // Swap list[i] with list[currentMinIndex] if necessary;
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    /**
+     * Print an array of objects
+     */
+    public static void printList(Object[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+
