924112cb77374f02e2c1c7354ab12e8551d15950
Mr <lsjsss@live.cn>
2020-04-21 15:30:29 +0800

    4.21 15 - update 4.21 class

--- src/java_Unit19/java19_9_SuperWildCardDemo.java
+++ src/java_Unit19/java19_9_SuperWildCardDemo.java
@@ -12,10 +12,18 @@ public class java19_9_SuperWildCardDemo {
         java19_8_AnyWildCardDemo.print(stack2);
     }
 
+    /** 下限通配   java19_1_GenericStack<T> <=> String , java19_1_GenericStack<? super T> <=> Object */
     public static <T> void add(java19_1_GenericStack<T> stack1, java19_1_GenericStack<? super T> stack2) {
         while (!stack1.isEmpty()) {
             stack2.push(stack1.pop());
         }
     }
+
+    /** 受限通配 */
+//    public static <T> void add(java19_1_GenericStack<T> stack1, java19_1_GenericStack<? super T> stack2) {
+//        while (!stack1.isEmpty()) {
+//            stack2.push(stack1.pop());
+//        }
+//    }
 }
 
