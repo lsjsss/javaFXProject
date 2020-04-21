a4c01b49ad485588bfc92296947fd0cc7b11a40f
Mr <lsjsss@live.cn>
2020-04-21 15:17:33 +0800

    4.21 15 - update 4.21 class

--- src/java_Unit19/java19_7_WildCardNeedDemo.java
+++ src/java_Unit19/java19_7_WildCardNeedDemo.java
@@ -10,7 +10,7 @@ public class java19_7_WildCardNeedDemo {
 //        System.out.print("The max number is " + max(intStack));
     }
 
-
+    //泛型是它的子类，泛型栈不是它(java19_1_GenericStack<Integer>)的子类
     /** Find the maximum in a stack of numbers */
     public static double max(java19_1_GenericStack<Number> stack) {
         // Initialize max
