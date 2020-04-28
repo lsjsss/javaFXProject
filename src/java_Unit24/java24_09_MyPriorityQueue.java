1ebca307792476a1d55d6edbef5891dd46cd4bb0
Mr <lsjsss@live.cn>
2020-04-28 21:36:49 +0800

    4.28 21 - update Unit24

---
+++ src/java_Unit24/java24_09_MyPriorityQueue.java
@@ -0,0 +1,19 @@
+package java_Unit24;
+
+import chapter23.Heap;
+
+public class java24_09_MyPriorityQueue<E extends Comparable<E>> {
+  private Heap<E> heap = new Heap<E>();
+
+  public void enqueue(E newObject) {
+    heap.add(newObject);
+  }
+
+  public E dequeue() {
+    return heap.remove();
+  }
+
+  public int getSize() {
+    return heap.getSize();
+  }
+}
