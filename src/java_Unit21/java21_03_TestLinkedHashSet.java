7e81ad7b2d4ead6c232a9339ff0f791613e5c598
Mr <lsjsss@live.cn>
2020-05-18 15:40:56 +0800

    5.18 15 - update 5.18 class

--- src/java_Unit21/java21_03_TestLinkedHashSet.java
+++ src/java_Unit21/java21_03_TestLinkedHashSet.java
@@ -3,25 +3,26 @@ package java_Unit21;
 import java.util.*;
 
 public class java21_03_TestLinkedHashSet {
-  public static void main(String[] args) {
-    // 创建一个哈希集 Create a hash set
-    Set<String> set = new LinkedHashSet<>();
-    
-    // 将字符串添加到集合中 Add strings to the set
-    set.add("London");
-    set.add("Paris");
-    set.add("New York");
-    set.add("San Francisco");
-    set.add("Beijing");
-    set.add("New York");
+    public static void main(String[] args) {
+        // 创建一个哈希集 Create a hash set
+        Set<String> set = new LinkedHashSet<>();
 
-    System.out.println(set);
+        // 将字符串添加到集合中 Add strings to the set
+        set.add("London");
+        set.add("Paris");
+        set.add("New York");
+        set.add("San Francisco");
+        set.add("Beijing");
+        // 第二次重复添加，Set 中不允许存在重复元素
+        set.add("New York");
 
-    // 显示哈希集中的元素 Display the elements in the hash set
-    // 不是构造方法
-    Iterator<String> iterator = set.iterator();
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next());
+        System.out.println(set);
+
+        // 显示哈希集中的元素 Display the elements in the hash set
+        // 不是构造方法
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
-  }
 } 
