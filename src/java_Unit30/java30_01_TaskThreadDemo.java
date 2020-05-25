6bf92406deadfced5062a12c54210299375ee7b1
Mr <lsjsss@live.cn>
2020-05-25 14:46:55 +0800

    5.25 14 - update 5.25 class

--- src/java_Unit30/java30_01_TaskThreadDemo.java
+++ src/java_Unit30/java30_01_TaskThreadDemo.java
@@ -1,60 +1,89 @@
 package java_Unit30;
 
 public class java30_01_TaskThreadDemo {
-  public static void main(String[] args) {
-    // Create tasks
-    Runnable printA = new PrintChar('a', 100);
-    Runnable printB = new PrintChar('b', 100);
-    Runnable print100 = new PrintNum(100);
+    public static void main(String[] args) {
+        // 创建任务，确定三个任务对象 Create tasks
+        Runnable printA = new PrintChar('a', 100);
+        Runnable printB = new PrintChar('b', 100);
+        Runnable print100 = new PrintNum(100);
 
-    // Create threads
-    Thread thread1 = new Thread(printA);
-    Thread thread2 = new Thread(printB);
-    Thread thread3 = new Thread(print100);
+        // 创建三个线程对象，分别用三个任务对象做参数
+        Thread thread1 = new Thread(printA);
+        Thread thread2 = new Thread(printB);
+        Thread thread3 = new Thread(print100);
 
-    // Start threads
-    thread1.start();
-    thread2.start();
-    thread3.start();
-  }
+        // 启动线程 Start threads
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        // 就绪
+    }
 }
 
-// The task for printing a specified character in specified times
+/**
+ * 在指定时间内打印指定字符的任务
+ * The task for printing a specified character in specified times
+ */
 class PrintChar implements Runnable {
-  private char charToPrint; // The character to print
-  private int times; // The times to repeat
+    /**
+     * 要打印的字符
+     * The character to print
+     */
+    private char charToPrint;
+    /**
+     * 重复的时间
+     * The times to repeat
+     */
+    private int times;
 
-  /** Construct a task with specified character and number of
-   *  times to print the character
-   */
-  public PrintChar(char c, int t) {
-    charToPrint = c;
-    times = t;
-  }
-
-  /** Override the run() method to tell the system
-   *  what the task to perform
-   */
-  public void run() {
-    for (int i = 0; i < times; i++) {
-      System.out.print(charToPrint);
+    /**
+     * 构造一个具有指定字符和数量的任务
+     * 打印字符的时间
+     * Construct a task with specified character and number of
+     * times to print the character
+     */
+    public PrintChar(char c, int t) {
+        charToPrint = c;
+        times = t;
     }
-  }
+
+    /**
+     * 覆盖run（）方法以告知系统
+     * 要执行什么任务
+     * Override the run() method to tell the system
+     * what the task to perform
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < times; i++) {
+            System.out.print(charToPrint);
+        }
+    }
 }
 
-// The task class for printing number from 1 to n for a given n
+/**
+ * 给定n的从1到n打印数字的任务类
+ * The task class for printing number from 1 to n for a given n
+ */
 class PrintNum implements Runnable {
-  private int lastNum;
+    private int lastNum;
 
-  /** Construct a task for printing 1, 2, ... i */
-  public PrintNum(int n) {
-    lastNum = n;
-  }
-
-  /** Tell the thread how to run */
-  public void run() {
-    for (int i = 1; i <= lastNum; i++) {
-      System.out.print(" " + i);
+    /**
+     * 构造一个任务以打印1、2，... i
+     * Construct a task for printing 1, 2, ... i
+     */
+    public PrintNum(int n) {
+        lastNum = n;
     }
-  }
+
+    /**
+     * 告诉线程如何运行
+     * Tell the thread how to run
+     */
+    @Override
+    public void run() {
+        for (int i = 1; i <= lastNum; i++) {
+            System.out.print(" " + i);
+        }
+    }
 }
--- src/java_Unit30/java30_03_ExecutorDemo.java
+++ src/java_Unit30/java30_03_ExecutorDemo.java
@@ -3,16 +3,16 @@ package java_Unit30;
 import java.util.concurrent.*;
 
 public class java30_03_ExecutorDemo {
-  public static void main(String[] args) {
-    // Create a fixed thread pool with maximum three threads
-    ExecutorService executor = Executors.newFixedThreadPool(3);
+    public static void main(String[] args) {
+        // Create a fixed thread pool with maximum three threads
+        ExecutorService executor = Executors.newFixedThreadPool(3);
 
-    // Submit runnable tasks to the executor
-    executor.execute(new PrintChar('a', 100));
-    executor.execute(new PrintChar('b', 100));
-    executor.execute(new PrintNum(100));
+        // Submit runnable tasks to the executor
+        executor.execute(new PrintChar('a', 100));
+        executor.execute(new PrintChar('b', 100));
+        executor.execute(new PrintNum(100));
 
-    // Shut down the executor
-    executor.shutdown();
-  }
+        // Shut down the executor
+        executor.shutdown();
+    }
 }
