f7fb8237c20ce8b253d5753377e69f5c4073dcbe
Mr <lsjsss@live.cn>
2020-05-26 14:55:57 +0800

    5.26 14 - update 5.26 class

--- src/java_Unit30/java30_03_ExecutorDemo.java
+++ src/java_Unit30/java30_03_ExecutorDemo.java
@@ -4,15 +4,22 @@ import java.util.concurrent.*;
 
 public class java30_03_ExecutorDemo {
     public static void main(String[] args) {
-        // Create a fixed thread pool with maximum three threads
+        // 创建最多包含三个线程的固定线程池  Create a fixed thread pool with maximum three threads
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
-        // Submit runnable tasks to the executor
+        // 将可运行任务提交给执行者  Submit runnable tasks to the executor
         executor.execute(new PrintChar('a', 100));
         executor.execute(new PrintChar('b', 100));
         executor.execute(new PrintNum(100));
 
-        // Shut down the executor
+        // 关闭执行器  Shut down the executor
         executor.shutdown();
     }
 }
+/**
+ * 提高执行效率，把线程放到线程池；
+ * 1.创建线程池，使用 Executors.newFixedThreadPool() 创建线程池；
+ * 2.把线程放到线程池中，exe: 执行；
+ * 3.关闭线程池；
+ */
+
