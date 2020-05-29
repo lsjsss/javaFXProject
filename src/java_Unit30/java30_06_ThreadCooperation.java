be1a1504563a2590f88c33926f1a0fa880960fba
Mr <lsjsss@live.cn>
2020-05-29 11:22:25 +0800

    5.29 11 - update 5.29 class

--- src/java_Unit30/java30_06_ThreadCooperation.java
+++ src/java_Unit30/java30_06_ThreadCooperation.java
@@ -4,88 +4,109 @@ import java.util.concurrent.*;
 import java.util.concurrent.locks.*;
 
 public class java30_06_ThreadCooperation {
-  private static Account account = new Account();
+    private static Account account = new Account();
 
-  public static void main(String[] args) {
-    // Create a thread pool with two threads
-    ExecutorService executor = Executors.newFixedThreadPool(2);
-    executor.execute(new DepositTask());
-    executor.execute(new WithdrawTask());
-    executor.shutdown();
+    public static void main(String[] args) {
+        // Create a thread pool with two threads
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(new DepositTask());
+        executor.execute(new WithdrawTask());
+        executor.shutdown();
 
-    System.out.println("Thread 1\t\tThread 2\t\tBalance");
-  }
+        System.out.println("Thread 1\t\tThread 2\t\tBalance");
+    }
 
-  // A task for adding an amount to the account
-  public static class DepositTask implements Runnable {
-    public void run() {
-      try { // Purposely delay it to let the withdraw method proceed
-        while (true) {
-          account.deposit((int)(Math.random() * 10) + 1);
-          Thread.sleep(1000);
+    /**
+     * 向帐户添加金额的任务
+     * A task for adding an amount to the account
+     */
+    public static class DepositTask implements Runnable {
+        @Override
+        public void run() {
+            try { // Purposely delay it to let the withdraw method proceed
+                while (true) {
+                    account.deposit((int) (Math.random() * 10) + 1);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
-      }
-      catch (InterruptedException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  // A task for subtracting an amount from the account
-  public static class WithdrawTask implements Runnable {
-    public void run() {
-      while (true) {
-        account.withdraw((int)(Math.random() * 10) + 1);
-      }
-    }
-  }
-
-  // An inner class for account
-  private static class Account {
-    // Create a new lock
-    private static Lock lock = new ReentrantLock();
-
-    // Create a condition
-    private static Condition newDeposit = lock.newCondition();
-
-    private int balance = 0;
-
-    public int getBalance() {
-      return balance;
     }
 
-    public void withdraw(int amount) {
-      lock.lock(); // Acquire the lock
-      try {
-        while (balance < amount) {
-          System.out.println("\t\t\tWait for a deposit");
-          newDeposit.await();
+    /**
+     * 从帐户中扣除金额的任务
+     * A task for subtracting an amount from the account
+     */
+    public static class WithdrawTask implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                account.withdraw((int) (Math.random() * 10) + 1);
+            }
         }
-        balance -= amount;
-        System.out.println("\t\t\tWithdraw " + amount +
-          "\t\t" + getBalance());
-      }
-      catch (InterruptedException ex) {
-        ex.printStackTrace();
-      }
-      finally {
-        lock.unlock(); // Release the lock
-      }
     }
 
-    public void deposit(int amount) {
-      lock.lock(); // Acquire the lock
-      try {
-        balance += amount;
-        System.out.println("Deposit " + amount +
-          "\t\t\t\t\t" + getBalance());
+    /**
+     * 帐户的内部类
+     * An inner class for account
+     */
+    private static class Account {
+        // 创建一个新锁  Create a new lock
+        private static Lock lock = new ReentrantLock();
 
-        // Signal thread waiting on the condition
-        newDeposit.signalAll();
-      }
-      finally {
-        lock.unlock(); // Release the lock
-      }
+        // 创建一个条件实例  Create a condition
+        private static Condition newDeposit = lock.newCondition();
+
+        private int balance = 0;
+
+        public int getBalance() {
+            return balance;
+        }
+
+        /**
+         * 取款方法
+         */
+        public void withdraw(int amount) {
+            lock.lock(); // Acquire the lock
+            try {
+                // 余额小于取款的金额
+                while (balance < amount) {
+                    System.out.println("\t\t\t等待存款 Wait for a deposit");
+                    // 条件等待，使当前线程处于等待状态
+                    newDeposit.await();
+                }
+
+                // 将存款减去，得到新的余额
+                balance -= amount;
+                System.out.println("\t\t\tWithdraw " + amount +
+                        "\t\t" + getBalance());
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            } finally {
+                // 释放锁 Release the lock
+                lock.unlock();
+            }
+        }
+
+        /**
+         * 存款
+         */
+        public void deposit(int amount) {
+            // 获取锁  Acquire the lock
+            lock.lock();
+            try {
+                balance += amount;
+                System.out.println("Deposit " + amount +
+                        "\t\t\t\t\t" + getBalance());
+
+                // 信号线程等待条件  Signal thread waiting on the condition
+                // 存完后，会唤醒其他等待的线程
+                newDeposit.signalAll();
+            } finally {
+                // 释放锁  Release the lock
+                lock.unlock();
+            }
+        }
     }
-  }
 }
--- src/java_Unit30/java30_07_ConsumerProducer.java
+++ src/java_Unit30/java30_07_ConsumerProducer.java
@@ -1,98 +1,120 @@
 package java_Unit30;
 
-import java.util.concurrent.*;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class java30_07_ConsumerProducer {
-  private static Buffer buffer = new Buffer();
+    private static Buffer buffer = new Buffer();
 
-  public static void main(String[] args) {
-    // Create a thread pool with two threads
-    ExecutorService executor = Executors.newFixedThreadPool(2);
-    executor.execute(new ProducerTask());
-    executor.execute(new ConsumerTask());
-    executor.shutdown();
-  }
-
-  // A task for adding an int to the buffer
-  private static class ProducerTask implements Runnable {
-    public void run() {
-      try {
-        int i = 1;
-        while (true) {
-          System.out.println("Producer writes " + i);
-          buffer.write(i++); // Add a value to the buffer
-          // Put the thread into sleep
-          Thread.sleep((int)(Math.random() * 10000));
-        }
-      } catch (InterruptedException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  // A task for reading and deleting an int from the buffer
-  private static class ConsumerTask implements Runnable {
-    public void run() {
-      try {
-        while (true) {
-          System.out.println("\t\t\tConsumer reads " + buffer.read());
-          // Put the thread into sleep
-          Thread.sleep((int)(Math.random() * 10000));
-        }
-      } catch (InterruptedException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  // An inner class for buffer
-  private static class Buffer {
-    private static final int CAPACITY = 1; // buffer size
-    private java.util.LinkedList<Integer> queue =
-      new java.util.LinkedList<Integer>();
-
-    // Create a new lock
-    private static Lock lock = new ReentrantLock();
-
-    // Create two conditions
-    private static Condition notEmpty = lock.newCondition();
-    private static Condition notFull = lock.newCondition();
-
-    public void write(int value) {
-      lock.lock(); // Acquire the lock
-      try {
-        while (queue.size() == CAPACITY) {
-          System.out.println("Wait for notFull condition");
-          notFull.await();
-        }
-
-        queue.offer(value);
-        notEmpty.signal(); // Signal notEmpty condition
-      } catch (InterruptedException ex) {
-        ex.printStackTrace();
-      } finally {
-        lock.unlock(); // Release the lock
-      }
+    public static void main(String[] args) {
+        // 创建具有两个线程的线程池  Create a thread pool with two threads
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(new ProducerTask());
+        executor.execute(new ConsumerTask());
+        executor.shutdown();
     }
 
-    public int read() {
-      int value = 0;
-      lock.lock(); // Acquire the lock
-      try {
-        while (queue.isEmpty()) {
-          System.out.println("\t\t\tWait for notEmpty condition");
-          notEmpty.await();
+    /**
+     * 向缓冲区添加int的任务，内部类
+     * A task for adding an int to the buffer
+     */
+    private static class ProducerTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                int i = 1;
+                while (true) {
+                    System.out.println("Producer writes " + i);
+                    // 向缓冲区添加值  Add a value to the buffer
+                    buffer.write(i++);
+                    // 让线程进入睡眠状态  Put the thread into sleep
+                    Thread.sleep((int) (Math.random() * 10000));
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 用于从缓冲区读取和删除int的任务
+     * A task for reading and deleting an int from the buffer
+     */
+    private static class ConsumerTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("\t\t\t消费者阅读 Consumer reads " + buffer.read());
+                    // Put the thread into sleep
+                    Thread.sleep((int) (Math.random() * 10000));
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 缓冲区的内部类
+     * An inner class for buffer
+     */
+    private static class Buffer {
+        // 缓冲区大小  buffer size
+        private static final int CAPACITY = 1;
+        private java.util.LinkedList<Integer> queue =
+                new java.util.LinkedList<Integer>();
+
+        // 创建一个新锁  Create a new lock
+        private static Lock lock = new ReentrantLock();
+
+        // 创建两个条件  Create two conditions
+        private static Condition notEmpty = lock.newCondition();
+        private static Condition notFull = lock.newCondition();
+
+        public void write(int value) {
+            // 获取锁  Acquire the lock
+            lock.lock();
+            try {
+                while (queue.size() == CAPACITY) {
+                    System.out.println("等待不完整的条件  Wait for notFull condition");
+                    notFull.await();
+                }
+
+                queue.offer(value);
+                // 信号不为空状态  Signal notEmpty condition
+                notEmpty.signal();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            } finally {
+                // 释放锁  Release the lock
+                lock.unlock();
+            }
         }
 
-        value = queue.remove();
-        notFull.signal(); // Signal notFull condition
-      } catch (InterruptedException ex) {
-        ex.printStackTrace();
-      } finally {
-        lock.unlock(); // Release the lock
-        return value;
-      }
+        public int read() {
+            int value = 0;
+            // 获取锁  Acquire the lock
+            lock.lock();
+            try {
+                while (queue.isEmpty()) {
+                    System.out.println("\t\t\t等待notEmpty条件  Wait for notEmpty condition");
+                    notEmpty.await();
+                }
+
+                value = queue.remove();
+                // 信号未满  Signal notFull condition
+                notFull.signal();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            } finally {
+                // 释放锁  Release the lock
+                lock.unlock();
+                return value;
+            }
+        }
     }
-  }
 }
