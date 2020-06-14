10ca9c178202a39497f381ea7cad41e814271ba6
Mr <lsjsss@live.cn>
2020-06-14 19:33:55 +0800

    6.14 19 - update middleTest

---
+++ src/middleTest/Srlect20.java
@@ -0,0 +1,32 @@
+package middleTest;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Srlect20 extends Application {
+        @Override // Override the start method in the Application class
+        public void start(Stage primaryStage) {
+            StackPane pane = new StackPane();
+            Button bt1 = new Button("Java");
+            Button bt2 = new Button("Java");
+            Button bt3 = new Button("Java");
+            Button bt4 = new Button("Java");
+            pane.getChildren().addAll(bt1, bt2, bt3, bt4);
+
+            Scene scene = new Scene(pane, 200, 250);
+            primaryStage.setTitle("Test"); // Set the stage title
+            primaryStage.setScene(scene); // Place the scene in the stage
+            primaryStage.show(); // Display the stage
+        }
+
+        /**
+         * The main method is only needed for the IDE with limited JavaFX
+         * support. Not needed for running from the command line.
+         */
+        public static void main(String[] args) {
+            launch(args);
+        }
+}
