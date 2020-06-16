1dc12d3eb6069fee956a7f64d260a395d1f3b846
Mr <lsjsss@live.cn>
2020-06-17 00:10:17 +0800

    6.17 00 - update 6.16 class

--- src/java_Unit32/java32_01_SimpleJdbc.java
+++ src/java_Unit32/java32_01_SimpleJdbc.java
@@ -2,32 +2,35 @@ package java_Unit32;
 
 import java.sql.*;
 
+/** 需要添加 mysql-connector-java-5.1.26-bin.jar */
 public class java32_01_SimpleJdbc {
     public static void main(String[] args)
             throws SQLException, ClassNotFoundException {
-        // Load the JDBC driver
+        // 加载 JDBC 驱动程序
         Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Driver loaded");
+//        Class.forName("oracle.jdbc.driver.OracleDriver");
+        System.out.println("驱动程序已加载");
 
-        // Establish a connection
+        // 建立连接
         Connection connection = DriverManager.getConnection
                 ("jdbc:mysql://localhost/javabook", "scott", "tiger");
-        System.out.println("Database connected");
+        System.out.println("数据库已连接");
 
-        // Create a statement
+        // 建立陈述
         Statement statement = connection.createStatement();
 
-        // Execute a statement
+        // 执行语句,通过 statement 语句执行
         ResultSet resultSet = statement.executeQuery
                 ("select firstName, mi, lastName from Student where lastName "
                         + " = 'Smith'");
 
-        // Iterate through the result and print the student names
-        while (resultSet.next())
+        // 遍历结果并打印学生姓名
+        while (resultSet.next()) {
             System.out.println(resultSet.getString(1) + "\t" +
                     resultSet.getString(2) + "\t" + resultSet.getString(3));
+        }
 
-        // Close the connection
+        // 关闭连接
         connection.close();
     }
 }
